d65830bd96ab997c18ce028d2888f38fc2a2ca9d
kazesberger <klaus.azesberger@bearingpoint.com>
2019-01-15 19:00:40 +0100

    git workshop init commit

--- src/reveal/slides.cljs
+++ src/reveal/slides.cljs
@@ -10,117 +10,93 @@
 (def slides
   [
    [:section
-    [:h1 "TEx: Clojure"]
-    [:h3 "Advocating at Bearingpoint Technology Graz"]]
+    [:h1 "git workshop"]
+    [:h3 "new austrian coding school"]]
 
    [:section
-     [:aside {:class "notes"}
-      [:p "@functional: concise = default (kotlin/scala)"]
-      [:p "@mutable: transaction/channels"]
-      [:p "@REPL: grow your code having data loaded in an unbroken Stream"]
-      [:p "@REPL: interactive programming"]]
-
-     [:h2 "What is Clojure?"]
+     [:h2 "WhoAmI?"]
      (bulletpoints
-       [[:a {:href "img/dynamic-and-static-typing.jpg"} "dynamically typed"]
-        "functional"
-        "immutable by default"
-        [:a {:href "https://danlebrero.com/2018/11/26/repl-driven-development-immediate-feedback-for-you-backend/#content"} "REPL"]
-        "concurrency"
-        "data-tools"
-        "lazyness"
-        "ez application domain modeling"])]
+       ["Klaus Azesberger"
+        [:a {:href "https://twitter.com/PsychodelicDad"} "@PsychodelicDad"]
+        [:a {:href "https://github.com/kazesberger"} "kazesberger"]
+        "Tech Lead Platform Team @ Bearingpoint Technology Graz"])]
    [:section
-    [:h2 "Core Philosophy"]
+     [:h2 "git/VCS: why / purpose ?"]
+     (bulletpoints
+       ["Collaboration"
+        "Storing Versions"
+        "Travel in time and perspective"
+        "Unerstanding what happened"])]
+
+   [:section
+     [:h2 "parts of git"]
+     (bulletpoints
+       [[:a {:href "https://git-scm.com/about/staging-area"} "Working Dir"]
+        [:a {:href "https://git-scm.com/about/staging-area"} "Staging Area"]
+        [:a {:href "https://git-scm.com/about/staging-area"} "Repo(s)"]
+        "remote repositories"])]
+
+   [:section
+     [:h2 "KataCoda"]
+     [:h4 "Exercises until TBD"]]
+
+   [:section
+    [:h2 "IDE git support"]
+    [:h4 "a short tour"]]
+
+   [:section
+    [:h2 "collaboration / conflicts"]
+    [:h4 "Some Terms"]
     (bulletpoints
-      ["Data is simple"
-       "Small, composable libraries"
-       [:div [:b "Very"] " stable"]])]
+      ["clone"
+       "create a branch (this is not a fork)"
+       "integrate a branch"
+       "merge"
+       "fast forward merge"
+       "conflict"
+       "3-way-merge"
+       "rebase"
+       "fork"])]
 
    [:section
-     (note [:a {:href "https://rigsomelight.com/2014/05/01/interactive-programming-flappy-bird-clojurescript.html"}
-            "Writing reloadable code has traditionally been a daunting software requirement.
-            In an imperative system, it requires scrupulous thinking about how to persist state through behavior changes.
-            This often involved serializing the state out of stateful components and then reinitializing new components
-            with the serialized state."])
-
-     [:h2 "interactive programming?"]
-
-    ;; FIXME code format
-     [:p {:class "fragment"}
-        "(def counter (atom 0))\n\n(defn template [count]\n  [:div\n   [:h1 \"Counter: \" count]\n   [:p [:a { :href \"#\"\n             :onClick (fn [e]\n                        (.preventDefault e)\n                        (swap! counter inc))} \"increment\"]]])\n\n(defn render [comp]\n  (.renderComponent js/React\n                    (sablono/html comp)\n                    (.getElementById js/document \"main-area\")))\n\n(add-watch counter :renderer\n           (fn [_ _ _ n]\n             (render (template n))))\n\n;; ping the state to trigger the first render\n(reset! counter @counter)\n"]]
+     [:h2 "cheat sheets / docs / help"]
+     (bulletpoints
+       ["git --help"
+        "git <command> --help"
+        "stackoverflow / google"
+        "slack / community"])]
 
    [:section
-    [:h2 "ClojureScript"]
-    (bulletpoints
-      [[:i "CLJS rocks but JS reaches"]
-       "What you can do with JS you can do with CLJS"
-       [:a {:href "https://purelyfunctional.tv/article/react-vs-re-frame/" } "React / Reframe"]
-       [:div "Google Clo" [:b [:u "s"]] "ure Compiler"]
-       "shadow-cljs"
-       "Node.js"
-       "planck / lumo"])]
+     [:h2 "conflict demo"]
+     (bulletpoints
+       ["PR"
+        "Review"
+        "LGTM"
+        [:div [:i "Exercise"]]])]
+
 
    [:section
-    [:h2 "CLR (.net Runtime)"]
-    (bulletpoints ["eg. Unity3d (Arcadia)"])
-    [:p]
-    [:br]
-    [:br]
-    [:h5 {:class "fragment"} "Experimental:"
-     (bulletpoints [[:a {:href "http://try.clojerl.online/"} "clojerl (clj on Erlang aka Beam VM)"]])]]
+     [:h2 "Contribute!"]
+     (bulletpoints
+       ["docs"
+        "Issues / Testing"
+        "github-profile > CV"
+        "daily coding katas"
+        "#100DaysOfCode"])]
 
    [:section
-    [:h2 "General purpose lang?"]
-    (bulletpoints [
-                   "Everything Java (including libs and tools)"
-                   "Everything JS (including libs and tools)"
-                   "ML / NLP / Data Science"
-                   "Game development on various platforms (eg. Unity3D or again of course anything JS)"
-                   "Scripting"])]
-   [:section
-    [:section
-     [:h2 "General purpose lang alts"]]
-    [:section
-       [:h2 "Python"]
-       [:ul
-        [:li {:style "list-style-image: url('img/plus_icon.png')"} "libraries"]
-        [:li {:style "list-style-image: url('img/minus_icon.png')"} "interop / target platforms"]]]
-    [:section
-       [:h2 "JS"]
-       [:ul
-        [:li {:style "list-style-image: url('img/plus_icon.png')"} "libraries"]
-        [:li {:style "list-style-image: url('img/minus_icon.png')"} "libraries"]
-        [:li {:style "list-style-image: url('img/minus_icon.png')"} "interop / target platforms"]
-        [:li {:style "list-style-image: url('img/minus_icon.png')"} "fatigue (and in parts Fear of missing out aka FOMO)"]
-        [:li {:style "list-style-image: url('img/minus_icon.png')"} "so many possibilities to do it wrong"]]]
+     [:h2 "Configuration tweaks"]
+     (bulletpoints
+       ["pull.autostash"
+        "pull.autorebase"
+        "log alias / configs"
+        "difftool"
+        "default editor"
+        "github 2fa"])]
 
-    [:section
-       [:h2 "Go"]
-       [:p "tbh idk that much about go yet. i'd just list some bullet points here:"]
-       (bulletpoints ["libs"
-                      "interop"
-                      "wasm - but there are also wasm compiler libs in JS and thus for CLJS"])]
-    [:section
-       [:h2 "so we're a Java shop - quo vadis?"]
-       (bulletpoints ["Kotlin"
-                      "Go"
-                      "Elixir/Erlang"
-                      "Polyglot"
-                      "Rust"
-                      "ReasonML"])]]
    [:section
-    [:h2 "pre live coding Questions"]
-    [:p "5-10min question time frame (1-2 Questions)"]]
-   [:section
-    [:h2 "Links"]
-    (bulletpoints [
-                   [:a {:href "https://danlebrero.com/2018/11/26/repl-driven-development-immediate-feedback-for-you-backend/#content"} "REPL driven development"]
-                   [:a {:href "http://clojurescriptmadeeasy.com/blog/clojurescript-is-not-coffeescript.html"} "CLJS is not (like) coffeescript"]
-                   [:a {:href "https://2018.stateofjs.com/front-end-frameworks/overview/"} "State of JS: FE frameworks"]
-                   [:a {:href "https://purelyfunctional.tv/article/react-vs-re-frame/"} "6 Reacter-mistakes that Reframers avoid"]
-                   [:a {:href "http://wiki.c2.com/?BlubParadox"} "Blub Paradox"]
-                   [:a {:href "https://www.innoq.com/blog/st/2010/04/clojure-performance-guarantees/"} "CLJ collection performance"]])]])
+     [:h2 "advanced github features"]]])
+
 
 
 (defn all
